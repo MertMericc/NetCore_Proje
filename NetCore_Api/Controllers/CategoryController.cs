3d333b1dee7eb568042513e7415e33a7f91a57ba
MERT MERİÇ <mericasd2051@gmail.com>
2022-07-23 16:56:04 +0300

    api yazildi

---
+++ NetCore_Api/Controllers/CategoryController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCore_Api.DAL.ApiContext;
+using NetCore_Api.DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var c = new Context();
+            return Ok(c.Categories.ToList());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            using var c = new Context();
+            var value = c.Categories.Find(id);
+            if (value==null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(value);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CategoryAdd(Category p)
+        {
+            using var c = new Context();
+            c.Add(p);
+            c.SaveChanges();
+            return Created("", p);
+        }
+
+        [HttpDelete]
+        public IActionResult CategoryDelete(int id)
+        {
+            using var c = new Context();
+            var value = c.Categories.Find(id);
+            if (value==null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                c.Remove(value);
+                c.SaveChanges();
+                return NoContent();
+            }
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCategory(Category p)
+        {
+            using var c = new Context();
+            var value = c.Find<Category>(p.CategoryID);
+            if (value==null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                value.CategoryName = p.CategoryName;
+                c.Update(value);
+                c.SaveChanges();
+                return NoContent();
+            }
+        }
+    }
+}
---
+++ NetCore_Api/DAL/ApiContext/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCore_Api.DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore_Api.DAL.ApiContext
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-NODMT1N; database=BlogProjeDB2; integrated security=true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
